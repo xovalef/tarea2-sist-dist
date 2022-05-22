608837a072b1016c2ba667fbbc7ed7ae7866e07a
rolitoxdd <rolo.alv97@gmail.com>
2022-05-22 01:34:29 -0400

    aaaAAAAaAASDLKJSALKDJ

--- login/index.js
+++ login/index.js
@@ -1,52 +1,58 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const users = require('./users')
+const express = require("express");
+const fs = require("fs/promises");
+const app = express();
+app.use(express.json());
+const users = require("./users");
 
-const { Kafka } = require('kafkajs')
+const { Kafka } = require("kafkajs");
 
+console.log("KAFKA_HOST: ", process.env.KAFKA_HOST);
 const kafka = new Kafka({
-  clientId: 'login',
-  brokers: [`${process.env.KAFKA_HOST}:9092`]
-})
+  clientId: "login",
+  brokers: [`${process.env.KAFKA_HOST}:9092`],
+});
 
-const producer = kafka.producer()
+const producer = kafka.producer();
 
+app.post("/login", async function (req, res) {
+  const user = req.body.user;
+  const pass = req.body.pass;
+  const blockedUsersFile = await fs.readFile("/blockedUsers.json", "utf-8");
+  const blockedUsers = JSON.parse(blockedUsersFile);
+  if (blockedUsers.find((blockedUser) => blockedUser == user)) {
+    return res.json({ login: false, error: "user blocked" });
+  }
 
-app.post('/login', async function(req, res){
-    const user=req.body.user
-    const pass=req.body.pass
-    const validar = validacion(user,pass)
+  const validar = validacion(user, pass);
 
-    await producer.connect()
-    await producer.send({
-      topic: 'login',
-      messages: [
-        { value: JSON.stringify({
-            user,
-            validation:validar
-        })},
-      ],
-    })
+  await producer.connect();
+  await producer.send({
+    topic: "login",
+    messages: [
+      {
+        value: JSON.stringify({
+          user,
+          validation: validar,
+        }),
+      },
+    ],
+  });
 
-    if(validar){
-        res.json({login:true})
-    }
-    else{
-        res.status(401).json({login:false})
-    }
-    //COSO KAFKA https://www.npmjs.com/package/kafkajs
-})
+  if (validar) {
+    res.json({ login: true });
+  } else {
+    res.status(401).json({ login: false, error: "incorrect credentials" });
+  }
+});
 
-function validacion(user,pass){
-    const userData= users.find(x=>x.user == user)
-    const passData= userData?.pass
-    if (userData.user==user && passData==pass){
-       return true 
-    }
-    else{
-        return false
-    }
+function validacion(user, pass) {
+  const userData = users.find((x) => x.user == user);
+  const passData = userData?.pass;
+  if (userData?.user == user && passData == pass) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-app.listen(3000)
+app.listen(3000);
--- security/index.js
+++ security/index.js
@@ -1,36 +1,65 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const usersBlocked = []
+const express = require("express");
+const fs = require("fs/promises");
+const app = express();
+app.use(express.json());
+const incorrectLogins = {};
 
-const { Kafka } = require('kafkajs')
+const { Kafka } = require("kafkajs");
 
 const kafka = new Kafka({
-  clientId: 'security',
-  brokers: [`${process.env.KAFKA_HOST}:9092`]
-})
-const consumer = kafka.consumer({ groupId: 'security-group' })
+  clientId: "security",
+  brokers: [`${process.env.KAFKA_HOST}:9092`],
+});
 
+const consumer = kafka.consumer({ groupId: "security-group" });
 
-app.get ('/blocked',async function(req,res){
-    res.json({
-        'users-blocked': usersBlocked
-    })
-})
+app.get("/blocked", async function (req, res) {
+  const blockedUsersFile = await fs.readFile("/blockedUsers.json", "utf-8");
+  const blockedUsers = JSON.parse(blockedUsersFile);
+  res.json({
+    "users-blocked": blockedUsers,
+  });
+});
 
-app.listen (5000, async function(){
-    await consumer.connect()
-    await consumer.subscribe({ topic: 'login', fromBeginning: true })
-    console.log('listening in port 5000')
+app.listen(5000, async function () {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "login", fromBeginning: true });
+  console.log("listening in port 5000");
 
-    await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
-          console.log({
-            partition,
-            offset: message.offset,
-            value: message.value,
-          })
-        },
-      })
-})
-
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const value = JSON.parse(message.value.toString());
+      const { timestamp } = message;
+      const { user, validation } = value;
+      const blockedUsersFile = await fs.readFile("/blockedUsers.json", "utf-8");
+      const blockedUsers = JSON.parse(blockedUsersFile);
+      if (blockedUsers.find((blockedUser) => blockedUser == user)) {
+        return;
+      }
+      if (!validation) {
+        if (!incorrectLogins[user]) {
+          incorrectLogins[user] = [Number(timestamp)];
+        } else {
+          incorrectLogins[user].push(Number(timestamp));
+          const incorrectLoginsLength = incorrectLogins[user].length;
+          console.log(incorrectLogins);
+          if (incorrectLoginsLength >= 5) {
+            if (
+              incorrectLogins[user][incorrectLoginsLength - 1] -
+                incorrectLogins[user][0] <=
+              60 * 1000
+            ) {
+              blockedUsers.push(user);
+              const data = await fs.writeFile(
+                "/blockedUsers.json",
+                JSON.stringify(blockedUsers)
+              );
+            } else {
+              incorrectLogins[user].shift();
+            }
+          }
+        }
+      }
+    },
+  });
+});
